cdfd8f955a308198d934b49ffa432f9f03f09d60
levente <levente@67hit.com>
2024-08-19 17:52:42 -0500

    Add bat file to run as a Windows service.

---
+++ wiki-actions/service-config.bat
@@ -0,0 +1,5 @@
+set WIKIDIR=c:\Users\Levente\Projects\github\67hit\software-docs\notes-wiki
+set TIDDLYCMD=c:\Users\Levente\AppData\Roaming\npm\tiddlywiki.cmd
+set HOST=0.0.0.0
+set PORT=9001
+set SERVICENAME=TiddlySoftwareDocs
